46d6527d5a8908b0862dab317e73b0566f7415fe
RenatoP <renato08lasalle13@gmail.com>
2017-09-24 20:56:03 -0500

    py

--- Cholo_Fighter/Personaje.py
+++ Cholo_Fighter/Personaje.py
@@ -1,3 +1,6 @@
 class Personaje():
-    def __init__(self):
-        self.nombre =
+    def __init__(self, nombre, ataques[], vida, caracterImg):
+        self.nombre = nombre
+        self.ataques = ataques[]
+        self.vida = vida
+        self.caracterImg = caracterImg

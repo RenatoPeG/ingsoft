ee612cdcf31702c96ecb1ecd344c90fe0ddb0555
RenatoP <renato08lasalle13@gmail.com>
2017-09-26 16:46:41 -0500

    django

--- server/menu/models.py
+++ server/menu/models.py
@@ -4,6 +4,10 @@ from django.db import models
 class Personaje(models.Model):
     nombre = models.CharField(max_length=100)
     vida = models.IntegerField()
+    icono = models.CharField(max_length=500)
 
     def __str__(self):
-        return self.nombre + ' - ' + str(self.vida)
+        cad = self.nombre + ' - ' \
+              + str(self.vida) + ' - ' \
+              + str(self.icono)
+        return cad
--- server/menu/urls.py
+++ server/menu/urls.py
@@ -2,9 +2,12 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    # /music/
-    url(r'^$', views.index, name='index'),
+    # /menu/
+    url(r'^$', views.menu, name='index'),
 
-    # /music/712/
-    url(r'^(?P<personaje_id>[0-9]+)/$', views.detail, name='detail')
+    # /menu/712/
+    url(r'^(?P<personaje_id>[0-9]+)/$', views.detalle, name='detalle'),
+
+    # seleccion de personaje
+    url(r'^seleccion_personaje/$', views.seleccion_personaje, name='seleccion'),
 ]
